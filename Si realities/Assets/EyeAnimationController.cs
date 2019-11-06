ab1cff08a0ead143e45bb9485d7888a6c757dcef
Baso <baso@sisanda.net>
2019-11-06 09:33:32 +0200

    Added new script
    Added new script for eye animations

--- Si realities/Assets/EyeAnimationController.cs
+++ Si realities/Assets/EyeAnimationController.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class EyeAnimationController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animation EyeExternal;
+    //public GameObject ExpandBtn;
+    //public GameObject ContractBtn;
+
     void Start()
     {
-        
+        EyeExternal.Play("idle");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void Expand()
+    {
+        EyeExternal.Play("expand");
+        //ExpandBtn.SetActive(false);
+        //ContractBtn.SetActive(true);
+    }
+
+    public void Contract()
+    {
+        EyeExternal.Play("contract");
+        //ExpandBtn.SetActive(true);
+        //ContractBtn.SetActive(false);
+    }
+
 }
